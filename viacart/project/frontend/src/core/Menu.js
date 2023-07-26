a6a59f58db1623033e90a800350a65dac9b9d853
Shaheer Ahmed Farooqui <shaheerahmedfarooqui758@gmail.com>
2023-07-26 20:34:31 +0500

    Ecommerce Store

---
+++ viacart/project/frontend/src/core/Menu.js
@@ -0,0 +1,119 @@
+import React, { Fragment } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { signout, isAuthenticated } from "../auth";
+import { itemTotal } from "./cartHelpers";
+import Search from './Search';
+const isActive = (history, path) => {
+    if (history.location.pathname === path) {
+        return { color: "#ff9900" };
+    } else {
+        return { color: "#ffffff" };
+    }
+};
+
+const Menu = ({ history }) => (
+    <div >
+        <ul className="nav nav-tabs bg-primary">
+            <li className="nav-item">
+                <Link
+                    className="nav-link"
+                    style={isActive(history, "/")}
+                    to="/"
+                >
+                    Viacart
+                </Link>
+            </li>
+
+            <li className="nav-item">
+                <Link
+                    className="nav-link"
+                    style={isActive(history, "/shop")}
+                    to="/shop"
+                >
+                    Shop
+                </Link>
+            </li>
+
+            <li className="nav-item">
+                <Link
+                    className="nav-link"
+                    style={isActive(history, "/cart")}
+                    to="/cart"
+                >
+                    Cart{" "}
+                    <sup>
+                        <small className="cart-badge">{itemTotal()}</small>
+                    </sup>
+                </Link>
+            </li>
+
+
+            {isAuthenticated() && isAuthenticated().user.role === 0 && (
+                <li className="nav-item">
+                    <Link
+                        className="nav-link"
+                        style={isActive(history, "/user/dashboard")}
+                        to="/user/dashboard"
+                    >
+                        Dashboard
+                    </Link>
+                </li>
+            )}
+
+            {isAuthenticated() && isAuthenticated().user.role === 1 && (
+                <li className="nav-item">
+                    <Link
+                        className="nav-link"
+                        style={isActive(history, "/admin/dashboard")}
+                        to="/admin/dashboard"
+                    >
+                        Dashboard
+                    </Link>
+                </li>
+            )}
+
+            {!isAuthenticated() && (
+                <Fragment>
+                    <li className="nav-item">
+                        <Link
+                            className="nav-link"
+                            style={isActive(history, "/signin")}
+                            to="/signin"
+                        >
+                            Login
+                        </Link>
+                    </li>
+
+                    <li className="nav-item">
+                        <Link
+                            className="nav-link"
+                            style={isActive(history, "/signup")}
+                            to="/signup"
+                        >
+                            Register
+                        </Link>
+                    </li>
+                </Fragment>
+            )}
+
+            {isAuthenticated() && (
+                <li className="nav-item">
+                    <span
+                        className="nav-link"
+                        style={{ cursor: "pointer", color: "#ffffff" }}
+                        onClick={() =>
+                            signout(() => {
+                                history.push("/");
+                            })
+                        }
+                    >
+                        Logout
+                    </span>
+                </li>
+            )}
+
+        </ul>
+    </div>
+);
+
+export default withRouter(Menu);
